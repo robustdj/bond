0cbcd035631b4e6dbe7f959b8a5018b9e5b4254b
Derrick Camerino <derrick@l33t.local>
2011-01-25 14:52:54 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbLikes::Application.config.secret_token = '28afe4d38d1dccba17f820d4c613b9a8a53c8ccf7157aa6adb6b9728fb08ba5653c12da0df8fe123f6643d32f22b2a2a0723cc903244f91dce3e1c80cb64d1f9'
